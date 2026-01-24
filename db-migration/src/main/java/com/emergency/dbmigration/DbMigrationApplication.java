dfb7aa94fbd3a379be50ebf474f1c14c9453dd6e
良缘 <15949866+ly-31415926@user.noreply.gitee.com>
2026-01-24 18:22:08 +0800

    flyway成功创建数据库

--- db-migration/src/main/java/com/emergency/dbmigration/DbMigrationApplication.java
+++ db-migration/src/main/java/com/emergency/dbmigration/DbMigrationApplication.java
@@ -1,7 +1,11 @@
 package com.emergency.dbmigration;
 
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DbMigrationApplication {
@@ -9,5 +13,13 @@ public class DbMigrationApplication {
     public static void main(String[] args) {
         SpringApplication.run(DbMigrationApplication.class, args);
     }
-
+    // 关键：通过 CommandLineRunner 触发 Flyway
+    @Bean
+    public CommandLineRunner migrate(@Autowired Flyway flyway) {
+        return args -> {
+            System.out.println("开始执行 Flyway 数据库迁移...");
+            flyway.migrate();
+            System.out.println("Flyway 迁移完成！");
+        };
+    }
 }
