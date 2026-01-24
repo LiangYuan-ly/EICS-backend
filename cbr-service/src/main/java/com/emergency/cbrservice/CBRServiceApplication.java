6e2aaeea7523ab17a2c17df442f54e2cef3009da
良缘 <15949866+ly-31415926@user.noreply.gitee.com>
2026-01-24 16:44:46 +0800

    初始化项目

---
+++ cbr-service/src/main/java/com/emergency/cbrservice/CBRServiceApplication.java
@@ -0,0 +1,13 @@
+package com.emergency.cbrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CBRServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CBRServiceApplication.class, args);
+    }
+
+}
